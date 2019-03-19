e6038472ba9fb39b66464c559ddb6018af711537
Kozlov Nikita <kozlovzxc@gmail.com>
2019-03-19 22:29:32 +0300

    feat(index): add sample module and component

--- src/app/modules/index/index-routing.module.ts
+++ src/app/modules/index/index-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './components/index/index.component';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: './modules/index/index.module#IndexModule'
+    component: IndexComponent,
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class IndexRoutingModule { }
