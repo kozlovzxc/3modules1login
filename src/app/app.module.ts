8a7b672f6625019baf29af9f931641b47c4b8c7c
Kozlov Nikita <kozlovzxc@gmail.com>
2019-03-19 22:57:19 +0300

    feat(common): add nxgs and logger plugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgxsModule } from '@ngxs/store';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -13,6 +16,8 @@ import { LoginComponent } from './components/login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgxsModule,
+    NgxsLoggerPluginModule.forRoot({ disabled: environment.production }), // must be last nxgs import
   ],
   providers: [],
   bootstrap: [AppComponent],
