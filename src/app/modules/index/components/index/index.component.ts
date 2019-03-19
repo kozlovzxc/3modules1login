ffda78487589f6b9ab84612a14b034f1cbcde5bf
Kozlov Nikita <kozlovzxc@gmail.com>
2019-03-20 02:05:53 +0300

    feat(index): separate login and logout buttons

--- src/app/modules/index/components/index/index.component.ts
+++ src/app/modules/index/components/index/index.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngxs/store';
+import { Store, Select } from '@ngxs/store';
 import { Logout } from 'src/app/stores/auth.actions';
+import { AuthState } from 'src/app/stores/auth.state';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-index',
@@ -8,6 +10,8 @@ import { Logout } from 'src/app/stores/auth.actions';
   styleUrls: ['./index.component.scss'],
 })
 export class IndexComponent implements OnInit {
+
+  @Select(AuthState.authenticated) authenticated$: Observable<boolean>;
 
   constructor(
     private store: Store,
